9af9807fc7f22c36342615bdf8b95f80691a6255
Alexandre Campos Hermida <achermida@gmail.com>
2022-02-09 08:34:19 -0300

    Class 15 changes Adding hard written tasks to list

--- src/App.js
+++ src/App.js
@@ -2,15 +2,37 @@ import "./styles.css";
 import "./components/Navbar/Navbar";
 import Navbar from "./components/Navbar/Navbar";
 import TaskList from "./components/Tasklist/TaskList";
+import { useState } from "react";
+
+let idAcc = 0;
+const generateId = () => {
+  idAcc = idAcc + 1;
+  return idAcc;
+};
 
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (title, state) => {
+    console.log("Calling function in App and id " + idAcc);
+    const newTask = {
+      id: generateId(),
+      title,
+      state
+    };
+
+    setTasks((existingTasks) => {
+      return [...existingTasks, newTask];
+    });
+  };
+
   return (
     <div className="App">
       <Navbar />
-      <div>
-        <TaskList title="Pending" />
-        <TaskList title="On Going" />
-        <TaskList title="Done" />
+      <div className="container">
+        <TaskList title="Pending" onAddTask={addTask} tasks={tasks} />
+        {/* <TaskList title="On Going" />
+        <TaskList title="Done" /> */}
       </div>
     </div>
   );
--- src/components/Tasklist/TaskList.js
+++ src/components/Tasklist/TaskList.js
@@ -1,11 +1,28 @@
 import React from "react";
+import ProtTypes from "prop-types";
 import "./tasklist.css";
 
-export default function TaskList(props) {
+export default function TaskList({ title, onAddTask, tasks }) {
+  const addTask = () => {
+    console.log("Calling function in TaskList");
+    onAddTask("New Task!", "Pending");
+  };
+
   return (
     <div className="tasklist">
-      <div className="title">{props.title}</div>
-      <div className="content"></div>
+      <div className="title">{title}</div>
+      <div className="content">
+        {tasks.map((task) => {
+          return <div key={task.id}>{task.title}</div>;
+        })}
+      </div>
+      <button onClick={addTask}>Add Task</button>
     </div>
   );
 }
+
+TaskList.propTypes = {
+  title: ProtTypes.string.isRequired,
+  onAddTask: ProtTypes.func.isRequired,
+  tasks: ProtTypes.array.isRequired
+};
